7fdcc9c18ad0242a570ea7b5a7ff73abe26a35f6
Roman Sopov <roman.sopov@bpmobile.com>
2018-10-25 17:55:37 +0300

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,9 +5,13 @@ module.exports = {
   head: {
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Meta description' }
     ]
   },
+  css: [
+    '~/assets/css/kube.css'
+  ],
   serverMiddleware: [
     // body-parser middleware
     bodyParser.json(),
