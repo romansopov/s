0c3ab782b7db2c7dd87e79c4b2bbe47d9e7139b6
Roman Sopov <roman.sopov@bpmobile.com>
2018-11-06 19:38:45 +0300

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,10 +4,15 @@ module.exports = {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'Meta description' }
-    ]
+    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    script: [{
+      src: '/js/kube.js'
+    }]
   },
   css: [
     '~/assets/css/kube.css',
+    '~/assets/themes/kube.theme.css',
     '~/assets/css/site.css'
   ]
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,42 @@
 <template>
-  <nuxt-link to="/login/">Login</nuxt-link>
+  <div>
+    <nuxt-link to="/login/">Login</nuxt-link>
+    <button @click="popup">Popup</button>
+    <div id="mymodal" class="is-hidden">...</div>
+  </div>
 </template>
+
+<script>
+  export default {
+    beforeMount() {
+      // $K.init()
+      $K.init();
+      (function ($K) {
+        console.log($K)
+        $K.add('module', 'mymodule', {
+          init: function(app, context) {
+            console.log(this)
+            this.app = app;
+            // define service
+            this.modal = app.modal;
+          },
+          showModal: function() {
+            console.log(this)
+            this.modal.open({
+              target: '#mymodal',
+              title: 'My Modal'
+            });
+          }
+        });
+      })(Kube);
+    },
+    methods: {
+      popup () {
+        $K.app.modal.open({
+          target: '#mymodal',
+          title: 'My Modal'
+        });
+      }
+    }
+  }
+</script>
