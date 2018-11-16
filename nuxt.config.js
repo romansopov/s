8005ee3f3c6ea9a5ca09b7c38a3503cf6acf3c55
Roman Sopov <roman.sopov@bpmobile.com>
2018-11-16 18:43:06 +0300

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,9 +3,12 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Meta description' }
+      { hid: 'description', name: 'description', content: 'S.Site' }
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    link: [
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      { rel: "stylesheet", href: "https://fonts.googleapis.com/css?family=Open+Sans:400,400i,600,600i,700,700i" }
+    ],
     script: [{
       src: '/js/kube.js'
     }]
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,60 @@
 <template>
-  <div>
-    <nuxt-link to="/login/">Login</nuxt-link>
-    <button @click="popup">Popup</button>
-    <div id="mymodal" class="is-hidden">...</div>
-  </div>
+    <div>
+        <div class="navigation is-global is-wrapper">
+            <!--
+            <nuxt-link to="/login/">Login</nuxt-link>
+            <button @click="popup">Popup</button>
+            <div id="mymodal" class="is-hidden">...</div>
+            -->
+            <div class="is-navbar-container is-container-large is-stack-20">
+                <div class="is-brand">
+                    <b class="is-logo">S.Site</b>
+                    <a href="#" class="nav-toggle is-push-right-mobile is-shown-mobile icon-kube-menu"
+                       data-kube="toggle"
+                       title="Menu" data-target="#navbar" data-loaded="true"></a>
+                </div>
+                <div id="navbar" class="is-navbar is-hidden-mobile">
+                    <nav>
+                        <ul>
+                            <li>
+                                <nuxt-link to="/page-1/">Page 1</nuxt-link>
+                            </li>
+                            <li>
+                                <nuxt-link to="/page-2/">Page 2</nuxt-link>
+                            </li>
+                            <li>
+                                <nuxt-link to="/page-3/">Page 3</nuxt-link>
+                            </li>
+                            <li>
+                                <nuxt-link to="/page-4/">Page 4</nuxt-link>
+                            </li>
+                            <li>
+                                <nuxt-link to="/page-5/">Page 5</nuxt-link>
+                            </li>
+                        </ul>
+                    </nav>
+                    <nav class="is-push-right">
+                        <ul>
+                            <li>
+                                <nuxt-link to="/console/">Console</nuxt-link>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+
+        <div class="is-wrapper">
+            <div class="is-page is-container is-container-large is-stack-40">
+                <div class="is-sidebar is-hidden-mobile">
+                    left sidebar
+                </div>
+                <div class="is-content">
+                    content
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
